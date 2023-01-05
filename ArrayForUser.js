17306f832679aa210e207b4a4ec4f0beb40313df
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-05 12:17:28 +0200

    corrected homework

--- ArrayForUser.js
+++ ArrayForUser.js
@@ -1,39 +1,31 @@
-let arrLength = Number(prompt('Enter length array :',''))
-let stringOfUser = prompt('Enter the elements' +
+const arrLength = Number(prompt('Enter length array :',''))
+const stringOfUser = prompt('Enter the elements' +
     'for array separated by a space','')
-let arrayForUser = stringOfUser.split(' ')
-for (let j = 0; j < arrayForUser.length;) {
+const arrayForUser = stringOfUser.split(' ')
+for (let j = 0; j < arrayForUser.length; j++) {
     if (Number.isNaN(Number(arrayForUser[j]))) {
-        alert(`Your item ${j} is not a number`)
-        j++
+        alert(`Your item ${j + 1} is not a number`)
+        break
     }
-    j++
 }
 
-if (Number.isNaN(arrLength) || arrLength <= 0) {
+if (!Number.isNaN(arrLength) || arrLength > 0) {
+    if (arrLength === arrayForUser.length) {
+        alert(`Your array : ${arrayForUser}`)
+
+        function sortArray(a, b) {
+            return a > b ? 1 : b > a ? -1 : 0
+        }
+
+        arrayForUser.sort(sortArray)
+        alert(`Array with elements in ascending order : ${arrayForUser}`)
+        const newArrayForUser = arrayForUser.splice(1, 3)
+        alert(`Items to be deleted : ${newArrayForUser}`)
+        alert(`Remaining items : ${arrayForUser}`)
+    }
+}
+else {
     alert(`You have entered incorrect length array`)
 }
-if (arrLength > arrayForUser.length) {
-    let differenceLength = arrLength - arrayForUser.length
-    for (let i = 1; i <= differenceLength; arrayForUser.push(0)) {
-       i++
-    }
-}
-else if (arrLength < arrayForUser.length) {
-    alert(`You have entered too many items\n
-    Length array is: ${arrayForUser.length}`)
-}
-alert(`Your array : ${arrayForUser}`)
-
-function sortArray(a, b) {
-    return a > b ? 1 : b > a ? -1 : 0
-}
-arrayForUser.sort(sortArray)
-alert(`Array with elements in ascending order : ${arrayForUser}`)
-let newArrayForUser = arrayForUser.splice(1, 3)
-alert(`Items to be deleted : ${newArrayForUser}`)
-alert(`Remaining items : ${arrayForUser}`)
-
-
 
 
